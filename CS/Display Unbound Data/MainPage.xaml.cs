448d16adb3af42d405307cef45384c2dc4d4e631
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Display Unbound Data/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Grid;
+
+namespace Display_Unbound_Data {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.ItemsSource = ProductList.GetData();
+        }
+        void grid_CustomUnboundColumnData(object sender, GridColumnDataEventArgs e) {
+            if(e.IsGetData) {
+                int price = Convert.ToInt32(e.GetListSourceFieldValue("UnitPrice"));
+                int unitsOnOrder = Convert.ToInt32(e.GetListSourceFieldValue("Quantity"));
+                e.Value = price * unitsOnOrder;
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to Display Unbound Data
+
+
+<p>This example shows how to add an unbound column to the grid. This column should display the total price, calculated as follows: UnitPrice * Quantity.</p>
+
+<br/>
+
+
